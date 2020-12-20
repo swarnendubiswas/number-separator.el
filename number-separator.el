7fd43b076731e37313e8fa95a7e034f6ccf5a281
Jeff Filipovits <jrfilipovits@gmail.com>
2020-12-19 20:11:20 -0500

    fix decimals

--- number-separator.el
+++ number-separator.el
@@ -7,9 +7,7 @@
 (defcustom number-separator-decimal-char "." "Character separating integer from decimal value.")
 
 (defvar number-separator--font-lock-keyword
-  `((,(concat "[[:digit:]]\\{"
-	      (number-to-string number-separator-ignore-threshold)
-	      ",\\}\\.?[[:digit:]]*")
+  `((,"\\.?[[:digit:]]+"
      (0 (list
 	 'face nil
 	 'display (number-separator)))))
@@ -17,28 +15,31 @@
 
 (defun number-separator ()  
   "Fix the number and return a new string."
-  (let* ((number (match-string 0))
-	 (separator (if (string= "." number-separator-decimal-char)
-			;; `split-string' needs a regexp as a separator,
-			;; so convert a period to a regexp
-			"\\."
-		      number-separator-decimal-char))
-	 (split-number (save-match-data ;; `split-string' messes with match-data
-			 (split-string number separator)))
-	 (integer (car split-number))
-	 (fractional (cadr split-number)))
-    (concat 
-     (cl-loop for x from (- (length integer) number-separator-interval)
-	      downto 1
-	      by number-separator-interval
-	      do (setq integer
-		       (concat (substring integer 0 x)
-			       number-separator
-			       (substring integer x (length integer))))
-	      finally return integer)
-     (when fractional
-       (concat number-separator-decimal-char
-	       fractional)))))
+  (let ((number (match-string 0)))
+    (if (or (string-prefix-p "." number)
+	    (<= (length number) number-separator-ignore-threshold))
+	number
+      (let*((separator (if (string= "." number-separator-decimal-char)
+			   ;; `split-string' needs a regexp as a separator,
+			   ;; so convert a period to a regexp
+			   "\\."
+			 number-separator-decimal-char))
+	    (split-number (save-match-data ;; `split-string' messes with match-data
+			    (split-string number separator)))
+	    (integer (car split-number))
+	    (fractional (cadr split-number)))
+	(concat 
+	 (cl-loop for x from (- (length integer) number-separator-interval)
+		  downto 1
+		  by number-separator-interval
+		  do (setq integer
+			   (concat (substring integer 0 x)
+				   number-separator
+				   (substring integer x (length integer))))
+		  finally return integer)
+	 (when fractional
+	   (concat number-separator-decimal-char
+		   fractional)))))))
 
 (define-minor-mode number-separator-mode
   "Separate long numbers."
